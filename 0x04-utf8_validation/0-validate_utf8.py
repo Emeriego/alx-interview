502defaa49844d6dd11b23b1438320030f211fd6
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-06-27 12:11:04 +0100

    utf8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,38 +1,39 @@
 #!/usr/bin/python3
 """UTF-8 Validation"""
 
-def validUTF8(data):
-    """Determines if a given data set
-    represents a valid utf-8 encoding
+def is_valid_byte(byte):
+    """ Checks each byte of data.
+        valid if within 0 - 255
     """
-    number_bytes = 0
+    return 0x00 <= byte <= 0xFF
 
-    mask_1 = 1 << 7
-    mask_2 = 1 << 6
+def validUTF8(data):
+    """ Checks if a data is valid utf8 encoded
+        single byte: 0b0xxxxxxx no continuation byte
+        2 bytes: 0b110xxxxx cont byte must start with 10xxxxxx
+        3 bytes: 0b1110xxxx cont byte: 10xxxxxx
+        4 bytes: 0b11110xxx cont byte: 10xxxxxx
+        you get it?
+    """
 
-    for i in data:
+    n_bytes = 0
 
-        mask_byte = 1 << 7
+    for byte in data:
+        if not is_valid_byte(byte):
+            return False
 
-        if number_bytes == 0:
-
-            while mask_byte & i:
-                number_bytes += 1
-                mask_byte = mask_byte >> 1
-
-            if number_bytes == 0:
-                continue
-
-            if number_bytes == 1 or number_bytes > 4:
+        if n_bytes == 0:
+            if (byte >> 5) == 0b110:
+                n_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                n_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                n_bytes = 3
+            elif (byte >> 7):
                 return False
-
         else:
-            if not (i & mask_1 and not (i & mask_2)):
+            if (byte >> 6) != 0b10:
                 return False
+            n_bytes -= 1
 
-        number_bytes -= 1
-
-    if number_bytes == 0:
-        return True
-
-    return False
+    return n_bytes == 0
