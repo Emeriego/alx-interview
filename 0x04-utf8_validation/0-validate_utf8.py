ec69bc01164a607577b98c92d2bf93a9cf07cc1e
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-06-27 12:05:43 +0100

    utf8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,14 +1,23 @@
 #!/usr/bin/python3
 """UTF-8 Validation"""
 
-def validUTF8(data):
-    """ Checks if a data set is valid utf8 encoded
+def is_valid_byte(byte):
+    """ Checks each byte of data.
+        valid if within 0 - 255
     """
-    def is_valid_byte(byte):
-        return 0x00 <= byte <= 0xFF
-    
+    return 0x00 <= byte <= 0xFF
+
+def validUTF8(data):
+    """ Checks if a data is valid utf8 encoded
+        single byte: 0b0xxxxxxx no continuation byte
+        2 bytes: 0b110xxxxx cont byte must start with 10xxxxxx
+        3 bytes: 0b1110xxxx cont byte: 10xxxxxx
+        4 bytes: 0b11110xxx cont byte: 10xxxxxx
+        you get it?
+    """
+
     n_bytes = 0
-    
+
     for byte in data:
         if not is_valid_byte(byte):
             return False
