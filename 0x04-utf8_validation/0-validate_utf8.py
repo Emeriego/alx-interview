257c12dcc11fd65783be078883d6dacf47c65f3e
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-06-28 02:18:35 +0100

    corrections

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -16,24 +16,24 @@ def validUTF8(data):
         you get it?
     """
 
-    n_bytes = 0
+    continuation_bytes = 0
 
     for byte in data:
         if not is_valid_byte(byte):
             return False
 
-        if n_bytes == 0:
+        if continuation_bytes == 0:
             if (byte >> 5) == 0b110:
-                n_bytes = 1
+                continuation_bytes = 1
             elif (byte >> 4) == 0b1110:
-                n_bytes = 2
+                continuation_bytes = 2
             elif (byte >> 3) == 0b11110:
-                n_bytes = 3
+                continuation_bytes = 3
             elif (byte >> 7):
                 return False
         else:
             if (byte >> 6) != 0b10:
                 return False
-            n_bytes -= 1
+            continuation_bytes -= 1
 
-    return n_bytes == 0
+    return continuation_bytes == 0
